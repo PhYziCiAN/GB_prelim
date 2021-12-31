4963d18410b390f51acebc09d2028930ddfa0097
Zhernakov Andrei <a.s.jernakov@gmail.com>
2021-12-31 16:06:21 +0300

    filling created array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿int[] a = new int[10]; //array with 10 random numbers
 Random rnd=new Random();
+
+for(int i=0;i<10;i++)
+{
+    a[i] = rnd.Next(1,50);
+}
+for(int i = 0;i < 10; i++) //++i лучше
+{
