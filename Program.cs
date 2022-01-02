254c2166522dda9b7d2114b492334411cee4484a
Andrei Zhernakov <a.s.jernakov@gmail.com>
2022-01-02 12:22:13 +0300

    added dev2 algorithm pic

--- Program.cs
+++ Program.cs
@@ -2,10 +2,20 @@
 Random rnd=new Random();
 
 for(int i=0;i<10;i++)
-{
-    a[i] = rnd.Next(1,50);
-}
-for(int i = 0;i < 10; i++) //++i лучше
-{
-    Console.Write("  "+ a[i]);
-}
+    {
+        a[i] = rnd.Next(1,50);
+    }
+for(int i = 0;i < 10; i++) //show first array
+    {
+        Console.Write("  "+ a[i]);
+    }
+int[] b = new int[10];
+
+for (int j = 0; j < a.Length; j++)
+    {
+        if (a[j] % 2 ==0)
+        {
+            b[j] = a[j];
+            Console.WriteLine("\n" + b[j]); //show second array
+        }
+    }
